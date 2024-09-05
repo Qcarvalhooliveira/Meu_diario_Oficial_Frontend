05d21cceaf326e990f56919e0c5dada83404fee0
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-09-05 16:04:02 +0200

    Integration contact using axios

--- src/components/contact_page/index.tsx
+++ src/components/contact_page/index.tsx
@@ -1,16 +1,66 @@
+import { useState } from "react";
+import axios from "axios";
 import { ContactContainer, Title, Form, Input, Textarea, Button } from "./styles";
 
 export function Contact() {
-    return (
-      <ContactContainer>
-        <Title>Contato</Title>
-        <Form>
-          <Input type="text" placeholder="Nome" required />
-          <Input type="email" placeholder="Email" required />
-          <Textarea rows={5} placeholder="Mensagem" required></Textarea>
-          <Button type="submit">Enviar</Button>
-        </Form>
-      </ContactContainer>
-    );
-  }
-  
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [status, setStatus] = useState("");
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    
+    try {
+      const response = await axios.post("http://127.0.0.1:5000/contact", {
+        
+        name,
+        email,
+        message,
+      });
+
+      if (response.status === 200) {
+        setStatus("Mensagem enviada com sucesso!");
+        setName("");
+        setEmail("");
+        setMessage("");
+      } else {
+        setStatus("Falha ao enviar a mensagem. Por favor, tente novamente.");
+      }
+    } catch (error) {
+      console.error("Erro ao enviar a mensagem:", error);
+      setStatus("Ocorreu um erro. Por favor, tente novamente.");
+    }
+  };
+
+  return (
+    <ContactContainer>
+      <Title>Contato</Title>
+      <Form onSubmit={handleSubmit}>
+        <Input 
+          type="text" 
+          placeholder="Nome" 
+          value={name} 
+          onChange={(e) => setName(e.target.value)} 
+          required 
+        />
+        <Input 
+          type="email" 
+          placeholder="Email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+          required 
+        />
+        <Textarea 
+          rows={5} 
+          placeholder="Mensagem" 
+          value={message} 
+          onChange={(e) => setMessage(e.target.value)} 
+          required 
+        ></Textarea>
+        <Button type="submit">Enviar</Button>
+      </Form>
+      {status && <p>{status}</p>}
+    </ContactContainer>
+  );
+}
